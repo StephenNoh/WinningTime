19d38ac2d01ca7505841ccba9895ee259a2fe3ed
Stephen Noh <Stephen.Noh@gmail.com>
2022-09-13 10:38:49 -0400

    changed url for answer key

--- scripts.js
+++ scripts.js
@@ -169,7 +169,7 @@ function tweetLink() {
         <p>40 or below - May god have mercy on your team.</p>
         </div>
         <p></p>
-        <p><a href="https://docs.google.com/document/d/1TEn4jBTPnB1RcUFrxzD-v9k144Z8s98BLl7TL_1F3V0/edit?usp=sharing" target="_blank">See answers and explanations</a></p>
+        <p><a href="https://www.sportingnews.com/us/nba/news/nba-head-coach-crunch-time-coaching-quiz-test/zqjfsr9aev0kvg5c8pvkvky3" target="_blank">See answers and explanations</a></p>
         
       <button type="button" class="btn btn-primary btn-lg" id="restart-button" onclick="restartQuiz()">Restart Quiz</button>
         `;
